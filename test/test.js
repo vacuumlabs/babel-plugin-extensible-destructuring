d4e4d76c5e0429bd2033c5bf3567f0592800dfb7
Markéta Lisová <marketa@lisova.cz>
2015-07-27 23:11:15 +0200

    Modernizing tests code

--- test/test.js
+++ test/test.js
@@ -1,12 +1,13 @@
 "use strict";
 
-var fs = require("fs");
-var assert = require("assert");
-var babel = require("babel-core");
+import Plugin from '../src'
+import fs from 'fs'
+import assert from 'assert'
+import { transformFileSync } from 'babel-core'
 
 function test(name, dir, externalHelpers) {
 	it("should compile " + name, function () {
-		var actual = babel.transformFileSync("./test/fixtures/" + dir + "/actual.js", {
+		var actual = transformFileSync("./test/fixtures/" + dir + "/actual.js", {
 			plugins: [require('../src/index')],
 			blacklist: ['es6.destructuring'],
 			externalHelpers: externalHelpers
@@ -16,7 +17,7 @@ function test(name, dir, externalHelpers) {
 	});
 }
 
-describe("extensible-destructuring", function () {
+describe("extensible-destructuring", () => {
 
 	test("basic object destructuring", "object-basic", false);
 	test("custom object destructuring with string keys", "object-custom", false);
